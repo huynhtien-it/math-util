8ccaf3ee8553ef00fae7f66eba026971688f2a91
huynhtien-it <huynhtien29102001@gmail.com>
2021-03-26 15:07:55 +0700

    Update code to add recursion. GREEN 4EVER AGAIN

--- src/com/huynhtienit/mathutil/MathUtillity.java
+++ src/com/huynhtienit/mathutil/MathUtillity.java
@@ -14,16 +14,26 @@ public class MathUtillity {
     //vi giai thua tang cuc nhanh ve gia tri, 21 tràn kiểu long
     //ta sẽ khống chế n đầu vào của hàm từ 0..20
     //kiểu long: 10^18, 18 con số 0 tối đa
-    public static long getFactorial(int n)
+//    public static long getFactorial(int n)
+//    {
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid argument. n must be between 0..20");
+//        if (n == 0 || n == 1)
+//            return 1; //0! = 1! = 1 tinh luon cho nhan
+//        //den doan nay sure n tu 2..20
+//        long result = 1; //cố tình heng, lát tính tiếp
+//        for (int i = 2; i <= n; i++) 
+//            result = result * i;
+//        return result;
+//    }
+    
+        public static long getFactorial(int n)
     {
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid argument. n must be between 0..20");
         if (n == 0 || n == 1)
-            return 1; //0! = 1! = 1 tinh luon cho nhan
-        //den doan nay sure n tu 2..20
-        long result = 1; //cố tình heng, lát tính tiếp
-        for (int i = 2; i <= n; i++) 
-            result = result * i;
-        return result;
+            return 1; //0! = 1! = 1 tinh luon cho nhanh
+        // mình thừa biết n! = n * (n - 1)!
+        return n * getFactorial(n - 1);
     }
 }
